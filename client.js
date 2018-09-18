02cb43fc8fecd06e3950d9ec40d9b866865bc80e
Maksim <the.butterfly.incorporated@gmail.com>
2018-09-18 18:16:51 +0300

    Add suffle

--- client.js
+++ client.js
@@ -1,7 +1,9 @@
 // client.js
 const net = require('net');
+const fs = require('fs');
+const shuffle = require('shuffle-array');
 const port = 8124;
-let seed = 1;
+let array;
 
 const client = new net.Socket();
 
@@ -9,8 +11,17 @@ client.setEncoding('utf8');
 
 client.connect(port, function() {
   console.log('Connected');
-  client.write(process.argv[2])
-  client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n' + '\nConnected user with id' + ++seed);
+  fs.readFile('qa.json', (err, text) => {
+        if (!err) {
+            array = JSON.parse(text);
+            shuffle(array);
+            client.write('QA');
+        }
+        else {
+            console.log(err);
+        }
+    })
+  //client.write('\r\nHello, Server!\r\nLove,\r\nClient.\r\n');
 });
 
 client.on('data', function(data) {
@@ -21,3 +32,4 @@ client.on('data', function(data) {
 client.on('close', function() {
   console.log('Connection closed');
 });
+
--- server.js
+++ server.js
@@ -1,25 +1,34 @@
 // server.js
 const net = require('net');
+const fs = require('fs');
 const port = 8124;
 let seed = 1;
+const logger = fs.createWriteStream('client_'+ seed +'.txt');
 
 const server = net.createServer((client) => {
   console.log('Client connected');
-
+  client.id = seed++;
+  logger.write('client ' + client.id + ' disconnected\n');
   client.setEncoding('utf8');
 
   client.on('data', (data) => {
     console.log(data);
-      if (data == "QA"){
-          client.write('\r\nHello!\r\nRegards,\r\nServer\r\n' + '\nHello user with id' + ++seed + "\nACK");
-      }
+        if (data == "QA"){
+           //client.write('\r\nHello!\r\nRegards,\r\nServer\r\n' +  "\nClient write ACK");
+           logger.write(client.id + ' Data: ' + data);
+           let answer = Math.random() > 0.5 ? '1' : '0';
+           logger.write(' Answer: ' + answer + '\n');
+           client.write(answer);
+        }
         else {
           client.write("DEC");
           client.destroy();
         }
     });
 
-  client.on('end', () => console.log('Client disconnected'));
+  client.on('end', () => console.log('Client disconnected')){
+    logger.write('client ' + client.id + ' disconnected\n');
+  };
 });
 
 server.listen(port, () => {
